bac767e228d362830f1d1265ef4e1548d165e5b8
mdKamrul-h <kamrul.sociofi@gmail.com>
2026-03-14 18:47:04 +0600

    portfolio-page-v2

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: false,
-    quality: 90,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     remotePatterns: [
@@ -33,9 +32,9 @@ const nextConfig = {
     ],
   },
   experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
+    webpackBuildWorker: false,
+    parallelServerBuildTraces: false,
+    parallelServerCompiles: false,
   },
 }
 
